f184b02144fcd9e5509be2b5e1c3ab27b77f44e1
Diogo Cabral <1230603@isep.ipp.pt>
2024-04-30 09:12:07 +0100

    VFM Menu

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/authorization/AuthenticationController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/authorization/AuthenticationController.java
@@ -14,6 +14,8 @@ public class AuthenticationController {
     public static final String ROLE_ADMIN = "ADMINISTRATOR";
     public static final String ROLE_HRM = "HRM";
 
+    public static final String ROLE_VFM = "VFM";
+
     //private final ApplicationSession applicationSession;
     private final AuthenticationRepository authenticationRepository;
 
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/authorization/AuthenticationUI.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/authorization/AuthenticationUI.java
@@ -4,6 +4,7 @@ import pt.ipp.isep.dei.esoft.project.application.controller.authorization.Authen
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.AdminUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.HRMUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.MenuItem;
+import pt.ipp.isep.dei.esoft.project.ui.console.menu.VFMUI;
 import pt.ipp.isep.dei.esoft.project.ui.console.utils.Utils;
 import pt.isep.lei.esoft.auth.mappers.dto.UserRoleDTO;
 
@@ -47,6 +48,7 @@ public class AuthenticationUI implements Runnable {
         List<MenuItem> rolesUI = new ArrayList<>();
         rolesUI.add(new MenuItem(AuthenticationController.ROLE_ADMIN, new AdminUI()));
         rolesUI.add(new MenuItem(AuthenticationController.ROLE_HRM, new HRMUI()));
+        rolesUI.add(new MenuItem(AuthenticationController.ROLE_VFM, new VFMUI()));
 
         //TODO: Complete with other user roles and related RoleUI
         return rolesUI;
