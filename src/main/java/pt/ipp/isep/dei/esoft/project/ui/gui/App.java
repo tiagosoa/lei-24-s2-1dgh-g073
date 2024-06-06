e056bae3c98e58603e924c5a5bad1bd314ffe285
Tiago Soares <1231246@isep.ipp.pt>
2024-06-06 10:36:20 +0100

    Creation of GUI

--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/App.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/App.java
@@ -5,11 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import pt.ipp.isep.dei.esoft.project.config.Config;
+import pt.ipp.isep.dei.esoft.project.ui.Bootstrap;
+
+import java.io.IOException;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.run();
+        try {
+            Config config = new Config("config.properties");
+            String emailService = config.getProperty("email.service");
+            System.out.println("Email Service: " + emailService);
+
+            // Use the config values as needed
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginUI.fxml"));
         Pane root = loader.load();
 
